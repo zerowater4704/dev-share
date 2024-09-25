ae2ad6d13a12a7e6baf2e601cf74b7c84fdd6817
Tomu Maekawa <tomu.m1107@icloud.com>
2024-09-25 17:00:31 +0900

    create userAPI (DELETE, PUT)

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -80,3 +80,63 @@ export async function GET(req: Request) {
     })
   }
 }
+
+export async function PUT(req: Request) {
+  await connectDB()
+  const body = await req.json()
+  const id = body._id
+
+  try {
+    const updateUser = await userModel.findByIdAndUpdate(
+      { _id: id },
+      {
+        $set: body,
+      },
+      { new: true },
+    )
+
+    if (!updateUser) {
+      return new Response(JSON.stringify({ error: 'user not found' }), {
+        status: 404,
+        headers: { 'Content-Type': 'application/json' },
+      })
+    }
+
+    return new Response(JSON.stringify({ message: 'Post updated successfully', updateUser }), {
+      status: 200,
+      headers: { 'Content-Type': 'application/json' },
+    })
+  } catch (error) {
+    return new Response(JSON.stringify({ error }), {
+      status: 500,
+      headers: { 'Content-Type': 'application/json' },
+    })
+  }
+}
+
+export async function DELETE(req: Request) {
+  await connectDB()
+  const body = await req.json()
+
+  // URLクエリパラメータからuserIdを取得
+  const url = new URL(req.url)
+  const id = url.searchParams.get('id')
+
+  if (!id) {
+    return new Response('Error: Missing userId', { status: 400 })
+  }
+
+  try {
+    const deletePost = await userModel.findByIdAndDelete({ _id: id })
+
+    return new Response(JSON.stringify(deletePost), {
+      status: 200,
+      headers: { 'Content-Type': 'application/json' },
+    })
+  } catch (error) {
+    return new Response(JSON.stringify({ error }), {
+      status: 500,
+      headers: { 'Content-Type': 'application/json' },
+    })
+  }
+}
