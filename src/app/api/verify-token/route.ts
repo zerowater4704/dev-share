500b8d6d778ae8ae2846064153aeb597646ba4b1
Tomu Maekawa <tomu.m1107@icloud.com>
2024-09-27 22:55:25 +0900

    create feature relogin

---
+++ src/app/api/verify-token/route.ts
@@ -0,0 +1,19 @@
+import jwt from 'jsonwebtoken'
+import { NextResponse } from 'next/server'
+
+export async function POST(request: Request) {
+  const authHeader = request.headers.get('Authorization')
+
+  if (!authHeader) {
+    return NextResponse.json({ error: 'Token is missing' }, { status: 401 })
+  }
+
+  const token = authHeader.split(' ')[1]
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET!)
+    return NextResponse.json({ message: 'Token is valid', user: decoded })
+  } catch (error) {
+    return NextResponse.json({ error: 'Token is invalid' }, { status: 401 })
+  }
+}
